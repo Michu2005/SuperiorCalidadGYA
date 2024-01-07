ec8c6d5984cfa2e63e479abda007de6d373a399c
Michelle Vaca <michu.vb2005@gmail.com>
2024-01-06 23:21:28 -0500

    Version inicial

---
+++ src/main/java/com/gruposuperior/calidad/manofactura/ManofacturaApplication.java
@@ -0,0 +1,13 @@
+package com.gruposuperior.calidad.manofactura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManofacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManofacturaApplication.class, args);
+	}
+
+}
